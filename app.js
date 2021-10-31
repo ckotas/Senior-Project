48d41325c006f60e7f37257a3aeead247ad193f2
Truong Dang <tdang7@uncc.edu>
2021-10-31 17:47:29 -0400

    finished setting up database

--- app.js
+++ app.js
@@ -3,70 +3,9 @@ var app = express();
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
 var bodyParser = require('body-parser');
-const {Database, sql } = require("@leafac/sqlite");
+const db = require('./database/database');
 
-//Creates Database-uncomment if you don't have the "Drom-Buddy.db" file
-const db = new Database("Drom-Buddy.db");
-db.execute(sql`
-        CREATE TABLE "Users"(
-            "userId" INTEGER PRIMARY KEY AUTOINCREMENT,
-            "fName" TEXT NOT NULL,
-            "lName" TEXT NOT NULL,
-            "role" TEXT NOT NULL,
-            "email" TEXT NOT NULL,
-            "roomId" INTEGER,
-            FOREIGN KEY("roomId") REFERENCES "Room"("roomId")
-        );
-        CREATE TABLE "Announcement"(
-            "announcementId" INTEGER PRIMARY KEY AUTOINCREMENT,
-            "organizer" INTEGER NOT NULL,
-            "title" TEXT NOT NULL,
-            "type" TEXT NOT NULL,
-            "date" DATE NOT NULL,
-            "color" TEXT NOT NULL,
-            "sTime" TIME NOT NULL,
-            "eTime" TIME NOT NULL,
-            "all-day" BOOl NOT NULL,
-            "repeat" BOOl NOT NULL,
-            "description" TEXT NOT NULL,
-            FOREIGN KEY("organizer") REFERENCES Users("userId")
-        );
-        CREATE TABLE "Room"(
-            "roomId" INTEGER PRIMARY KEY AUTOINCREMENT,
-            "userId" VARCHAR,
-            "plannerId" VARCHAR
-        );
-        CREATE TABLE "Planner"(
-            "plannerId" INTEGER PRIMARY KEY AUTOINCREMENT,
-            "roomId" INTEGER NOT NULL,
-            "date" DATE NOT NULL,
-            "time" TIME NOT NULL,
-            "message" TEXT,
-            FOREIGN KEY("roomId") REFERENCES Room("roomId")
-        );
-        CREATE TABLE "Event"(
-            "eventId" INTEGER PRIMARY KEY AUTOINCREMENT,
-            "attendees" VARCHAR NOT NULL,
-            "organizer" INTEGER NOT NULL,
-            "date" DATE NOT NULL,
-            "time" TIME NOT NULL,
-            "location" TEXT NOT NULL,
-            "details" TEXT NOT NULL,
-            FOREIGN KEY("organizer") REFERENCES Users("userId")
-        );
-        CREATE TABLE "Message"(
-            "messageId" INTEGER PRIMARY KEY AUTOINCREMENT,
-            "recipient" INTEGER NOT NULL,
-            "sender" INTEGER NOT NULL,
-            "date" DATE NOT NULL,
-            "time" TIME NOT NULL,
-            "read" BOOl NOT NULL,
-            "anonymous" BOOL NOT NULL, 
-            "message" TEXT NOT NULL,
-            FOREIGN KEY("recipient") REFERENCES Users("userId"),
-            FOREIGN KEY("sender") REFERENCES Users("userId")
-        );
-        `);
+
 
 //Establishing session and cookies
 const session = require("express-session");
