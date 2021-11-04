75de10ed7285fdd8e7e3293af34d29ff7378a111
AdamHill1 <48526436+AdamHill1@users.noreply.github.com>
2021-11-04 17:09:05 -0400

    t

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const controller = require('../controller/mainController');
+const controller = require('./controller/mainController');
 
 const router = express.Router();
 
