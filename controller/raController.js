cdf0ae5c7a9e67b3aa9af97f6b3a2fe1f06aa012
Austin Hill <ahill112@uncc.edu>
2021-11-11 17:16:26 -0500

    update RA with create event and calendar view still not pulling every event

--- controller/raController.js
+++ controller/raController.js
@@ -1,5 +1,28 @@
+const db = require('../database/database');
+var {uid} = require('uid');
+
 exports.homepage = (req, res)=>{
-    res.render('HomePage');
+    let roomId = req.session.user.roomId
+    
+    let event = db.getEvents(roomId)
+    if(event == undefined){
+
+    } else {
+    if (event.repeat === 'None'){
+        delete event.daysOfWeek
+    } else if (event.repeat === 'Daily'){
+        event.daysOfWeek = ['0','1','2','3','4','5','6']
+        
+    }else if (event.repeat === 'Weekly'){
+        let temp = event.daysOfWeek;
+        event.daysOfWeek = [temp];
+    }
+    console.log(event)
+    
+}
+      
+      
+    res.render('HomePage', {event});
 };
 
 exports.inbox = (req, res)=>{
@@ -24,7 +47,17 @@ exports.createEventRa = (req, res)=>{
 };
 
 exports.createdEventRa = (req, res)=>{
-    res.redirect('homepage');
+    console.log(req.body);
+    //Get proper time format
+    var startDateTime = req.body.edate + "T" + req.body.eSTime + ":00";
+    var endDateTime = req.body.edate + "T" + req.body.eETime + ":00";
+    
+    //get day of week
+    const dayofweek = new Date(startDateTime);
+    const day = dayofweek.getDay();
+
+    db.addEvent(startDateTime, endDateTime, req.body.eColor, req.body.eTextcolor, day.toString(), req.body.eTitle, req.body.eDescription, req.body.eType, req.session.user.userId, req.session.user.roomId, req.body.edate, req.body.eRecurDateend, req.body.eRepeat)
+    res.redirect('HomePage');
 };
 
 exports.logoutRA = (req, res)=>{
